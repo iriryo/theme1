6b63b06b61a05755c8f00e26bb749913ed13d66b
iriryo <info@iriryo.com>
2018-12-19 18:19:17 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>ここにタイトルが入ります</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script/main.js"></script>
+    </head>
+    
+    <body>
+        
+    <div class="wrap">
+        <header>
+            <h1>ヘッダータイトル</h1>
+        </header>
+    </div>
+    
+    </body>
+    
+</html>
