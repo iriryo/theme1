dfe391da17d4f6347c9fc406d18c28ad4cb7c1c5
iriryo <info@iriryo.com>
2018-12-24 19:02:07 +0900

    third commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width">
         <title>ここにタイトルが入ります</title>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-        <script src="script/main.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/script/main.js"></script>
     </head>
     
     <body>
@@ -13,6 +13,15 @@
         <header>
             <h1>ヘッダータイトル</h1>
         </header>
+
+        <div class="contents">
+            本コンテンツ本コンテンツ本コンテンツ本コンテンツ本コンテンツ本コンテンツ
+        </div>
+
+        <footer>
+            &copy;著作権所有者名
+        </footer>
+
     </div>
     
     </body>
