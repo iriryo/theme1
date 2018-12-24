5d509dd847f8b0f269f434c6e2dc4b0aab19f510
iriryo <info@iriryo.com>
2018-12-24 18:50:30 +0900

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
         <title>ここにタイトルが入ります</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
         <script src="script/main.js"></script>
     </head>
     
